9b94a7e974103a52ad37f021dbace5c89131c8c1
devinrhode2 <devinrhode2@gmail.com>
2022-12-23 07:16:13 -0500

    copy over some basic postinstall git settings
    from at_las

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
 
 cp bun.lockb last-good.bun.lockb
+
+# Could use this approach, but it seems less secure:
+# git config --local include.path $(git rev-parse --show-toplevel)/.gitconfig || echo "not a git repo, skipping git blame config"
+# Also, if this code is embeded back into the package.json postinstall string,
+# it can fail if it's making certain assumptions about the terminal (i.e. that it's bash, etc)
+git config --local core.eol lf && \
+git config --local core.autocrlf false && \
+git config --local blame.ignoreRevsFile .git-blame-ignore-revs || echo "not a git repo, skipping git config"
